de613d81542258b4fb8e5c6f95cb737a4995c35a
umbelino1 <145998576+umbelino1@users.noreply.github.com>
2026-04-03 15:57:33 -0300

    Add files via upload

---
+++ catalogo/js/data - pereira.js
@@ -0,0 +1,41 @@
+export const categories = [
+    {
+        title: "Mais assitidos",
+        items: [
+            {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr_I_O9syiGinSFjo76xANBZ9Ft52uNZmdpg&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=ScRicG6fG5Y"
+            },
+            {
+                img: "https://rukminim2.flixcart.com/image/480/640/j83d8cw0/poster/p/k/a/large-transformers-3-dark-of-the-moon-movie-on-fine-art-paper-on-original-imaerxzcb9pazqnp.jpeg?q=90", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=wR-CovQuK0c"
+            },
+            {
+                img: "https://medias.revistaoeste.com/wp-content/uploads/2024/12/Eu-sou-a-Lenda-712x370.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=aF_-JDP3qmY"
+            },
+            {
+                img: "https://is1-ssl.mzstatic.com/image/thumb/Video211/v4/a3/0b/0f/a30b0f0c-9469-b5fe-10b4-eb98ded56c45/6827359c-8f74-4094-9c58-ca3a7a876e77_American_Sniper_4K_H_DD_KA_TT_CVRART_3840x2160_300dpi_BR-p3.lsr/1200x675.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=zWeMSMT53Gs"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/5d23/live/38be3b60-bc2f-11ee-8685-316409d66f25.jpg.webp", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=_WLP-0PvlbI" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSatVX9hPxEgg0K6MNWRkWbpF5miB_Q_3johQ&s", top10: true, youtube: "https://www.youtube.com/watch?v=dA2DgKYNMfU" },
+            { img: "https://www.tallengestore.com/cdn/shop/products/stranger_things_12_01151e69-a27e-42c3-b339-eb3afc33a939.jpg?v=1514020430", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=4DMUy20QfdM" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnzIRTodEd47BCNBe97_UCEWd3BN9CPMiJyQ&s", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=B_KSH6w-gps" },
+        ]
+    },
+    {
+        title: "Recomendações para você",
+        items: [
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz5S6COIhcpIVFKW3fHrKTYuW2stH3w_ALXQ&s", top10: true, youtube: "https://www.youtube.com/watch?v=RWSuAC9CYxo" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg2HcYyWuPRP76rivbWAkkHxA3Mf9-Bk2GsQ&s", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=_8P8MgsFNZQ" },
+            { img: "https://i.ytimg.com/vi/JkoSAwJUwAo/maxresdefault.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=D3ILZI3ZCpQ" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUhxSRu02osf8wdbnbUkaDIfu0n0g0oXpw-A&s", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=Ofm1LoilSK4" },
+        ]
+    }
+];
